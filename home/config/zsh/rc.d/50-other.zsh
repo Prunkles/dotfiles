3fb18be91cf6803f3b54b9bfa4663fb9605cd49b
Prunkles <prunkles.dreemurr@gmail.com>
2023-12-24 09:03:43 +0200

    fix: Fix starship wrong prompt width

--- home/config/zsh/rc.d/50-other.zsh
+++ home/config/zsh/rc.d/50-other.zsh
@@ -10,6 +10,8 @@ alias ls="eza"
 
 alias lg="lazygit"
 
+export LANG=C.UTF-8 # https://github.com/nix-community/home-manager/issues/3711#issuecomment-1748480665
 eval "$(starship init zsh)"
+
 eval "$(zoxide init zsh)"
 
