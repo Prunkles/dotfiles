3b89ea03f3ba7bf6947866aa64f608299930a54c
Prunkles <prunkles.dreemurr@gmail.com>
2023-12-24 08:38:46 +0200

    fix: Create zsh history directory if not exists

--- home/config/zsh/rc.d/80-xdg-ninja-antidot.zsh
+++ home/config/zsh/rc.d/80-xdg-ninja-antidot.zsh
@@ -3,8 +3,10 @@
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 # [go]: $HOME/go
 export GOPATH="$XDG_DATA_HOME"/go
-# [zsh]: $HOME/.zshenv
+# [zsh]: $HOME/.zsh_history
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 # [postgresql]: $HOME/.psql_history
 export PSQL_HISTORY="$XDG_DATA_HOME/psql_history"
+
+mkdir -p "$(dirname "$HISTFILE")"
 
