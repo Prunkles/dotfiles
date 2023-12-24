1d998770f34a9adb83927e6df40c8e2d22005b41
Prunkles <prunkles.dreemurr@gmail.com>
2023-12-24 08:26:47 +0200

    Move cli-common configuration to a separate module

---
+++ home/cli-common.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, ... }:
+{
+  home.packages = [
+    pkgs.zsh
+    pkgs.oh-my-zsh
+    pkgs.zsh-autosuggestions
+    pkgs.zsh-syntax-highlighting
+    pkgs.nix-zsh-completions
+
+    pkgs.bottom
+    pkgs.bat
+    pkgs.eza
+    pkgs.zoxide
+    pkgs.starship
+    pkgs.lazygit
+
+    pkgs.tmux
+    pkgs.tmuxPlugins.sensible
+    pkgs.tmuxPlugins.catppuccin
+    pkgs.tmuxPlugins.vim-tmux-navigator
+
+    pkgs.neovim
+    pkgs.lua-language-server # For neovim Lua LSP
+  ];
+
+  home.activation = {
+    checkNvimLazyNvimLock = lib.hm.dag.entryBefore ["linkGeneration"] ''
+      lazyNvimLock="$HOME/.local/share/nvim/lazy/lazy-lock.json"
+      if [ -f "$lazyNvimLock" ]; then
+          if ! cmp --silent "${./config/nvim/lazy-lock.json}" "$lazyNvimLock"; then
+              _iError "lazy.nvim lock file at '$lazyNvimLock' already exists and differs from expected. Remove it or update it in dotfiles"
+              exit 1
+          fi
+      fi
+    '';
+    writeNvimLazyNvimLock = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD cp $VERBOSE_ARG --no-preserve=mode "${./config/nvim/lazy-lock.json}" "$lazyNvimLock"
+    '';
+  };
+
+  home.file = {
+    ".zshrc".source = ./zshrc;
+  };
+  xdg.configFile = {
+    "zsh".source = ./config/zsh;
+    "nvim".source = ./config/nvim;
+    "tmux".source = ./config/tmux;
+  };
+}
+
--- home/home.nix
+++ home/home.nix
@@ -15,66 +15,12 @@
 
   targets.genericLinux.enable = true;
 
-  home.activation = {
-    checkNvimLazyNvimLock = lib.hm.dag.entryBefore ["linkGeneration"] ''
-      lazyNvimLock="$HOME/.local/share/nvim/lazy/lazy-lock.json"
-      if [ -f "$lazyNvimLock" ]; then
-          if ! cmp --silent "${./config/nvim/lazy-lock.json}" "$lazyNvimLock"; then
-              _iError "lazy.nvim lock file at '$lazyNvimLock' already exists and differs from expected. Remove it or update it in dotfiles"
-              exit 1
-          fi
-      fi
-    '';
-    writeNvimLazyNvimLock = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      $DRY_RUN_CMD cp $VERBOSE_ARG --no-preserve=mode "${./config/nvim/lazy-lock.json}" "$lazyNvimLock"
-    '';
-  };
-
-  home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-
-    pkgs.zsh
-    pkgs.oh-my-zsh
-    pkgs.zsh-autosuggestions
-    pkgs.zsh-syntax-highlighting
-    pkgs.nix-zsh-completions
-
-    pkgs.bottom
-    pkgs.bat
-    pkgs.eza
-    pkgs.zoxide
-    pkgs.starship
-    pkgs.lazygit
-
-    pkgs.tmux
-    pkgs.tmuxPlugins.sensible
-    pkgs.tmuxPlugins.catppuccin
-    pkgs.tmuxPlugins.vim-tmux-navigator
-
-    pkgs.neovim
-    pkgs.lua-language-server # For neovim Lua LSP
+  imports = [
+    ./cli-common.nix
   ];
 
-  home.file = {
-    ".profile".source = ./profile;
-    ".zshrc".source = ./zshrc;
-  };
   xdg.configFile = {
     "nix".source = ./config/nix;
-    "zsh".source = ./config/zsh;
-    "nvim".source = ./config/nvim;
-    "tmux".source = ./config/tmux;
   };
 
   # Let Home Manager install and manage itself.
