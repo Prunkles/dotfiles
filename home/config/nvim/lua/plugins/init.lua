46f007386c87028b8592eca8aa3d0df96f6a00d2
Prunkles <prunkles.dreemurr@gmail.com>
2024-01-24 06:18:22 +0200

    neovim: Add a bit fugitive

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -28,6 +28,7 @@ return {
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
         end
     },
 
