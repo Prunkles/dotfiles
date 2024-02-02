40aa2f2e69c66f31d45b2da7e541b1b395af0484
Prunkles <prunkles.dreemurr@gmail.com>
2024-02-02 03:44:26 +0200

    zsh: Change fpath nix init to the default one in zsh from home-manager

--- home/config/zsh/rc.d/09-nix.zsh
+++ home/config/zsh/rc.d/09-nix.zsh
@@ -1,8 +1,9 @@
 
-fpath=(
-    ~/.nix-profile/share/zsh/site-functions
-    "${fpath[@]}"
-)
+. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+for profile in ${(z)NIX_PROFILES}; do
+  fpath+=($profile/share/zsh/site-functions $profile/share/zsh/$ZSH_VERSION/functions $profile/share/zsh/vendor-completions)
+done
 
 source ~/.nix-profile/share/zsh/plugins/nix/init.zsh
 
