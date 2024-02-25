7a2d6d47fe41958921dd4733a2785bc356a30956
Prunkles <prunkles.dreemurr@gmail.com>
2024-02-25 05:27:11 +0200

    nvim: Add `oil.nvim` plugin for a split explorer

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -58,5 +58,15 @@ return {
         'nvim-tree/nvim-web-devicons'
     },
 
+    {
+        'stevearc/oil.nvim',
+        opts = {
+            view_options = {
+                show_hidden = true,
+            },
+        },
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
+
 }
 
