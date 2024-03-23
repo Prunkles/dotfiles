eba665d85be838b7b78d61a283dde49d9ef5661b
Prunkles <prunkles.dreemurr@gmail.com>
2024-03-23 22:10:30 +0200

    nvim: Add neo-tree

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -68,5 +68,15 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
     },
 
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v3.x',
+        dependencies = {
+          'nvim-lua/plenary.nvim',
+          'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+          'MunifTanjim/nui.nvim',
+          -- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+    },
 }
 
