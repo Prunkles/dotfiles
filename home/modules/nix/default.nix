2ed2447d819a30fce381d57d099211e1c48a6abe
Prunkles <prunkles.dreemurr@gmail.com>
2024-08-03 23:53:21 +0300

    Update flake inputs

--- home/modules/nix/default.nix
+++ home/modules/nix/default.nix
@@ -1,7 +1,7 @@
 { pkgs, nixpkgs, ... }:
 {
   nix = {
-    package = pkgs.nixVersions.nix_2_18;
+    package = pkgs.nixVersions.stable;
     settings.experimental-features = "nix-command flakes repl-flake";
     registry = {
       "nixpkgs".flake = nixpkgs;
