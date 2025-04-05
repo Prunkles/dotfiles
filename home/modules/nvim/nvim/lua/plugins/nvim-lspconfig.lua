18d42217e61a632e2bc8b240a431c3d67ad16df3
Prunkles <prunkles.dreemurr@gmail.com>
2025-04-05 09:20:41 +0300

    nvim: refactor(lspconfig): Specify mode explicitly for each map

--- home/modules/nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/modules/nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -30,30 +30,30 @@ return {
                 -- Enable completion triggered by <c-x><c-o>
                 vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-                local nmap = function(keys, func, desc)
+                local map = function(mode, keys, func, desc)
                     if desc then
                         desc = 'LSP: ' .. desc
                     end
-                    vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc })
+                    vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = desc })
                 end
 
-                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-                nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction') -- TODO: v mode
+                map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                map('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction') -- TODO: v mode
 
-                nmap('gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
-                nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-                nmap('gr', telescope_builtin.lsp_references, '[G]oto [R]erefences')
-                nmap('gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
-                nmap('<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
-                nmap('<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-                nmap('<leader>ws', telescope_builtin.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+                map('n', 'gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
+                map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                map('n', 'gr', telescope_builtin.lsp_references, '[G]oto [R]erefences')
+                map('n', 'gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
+                map('n', '<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
+                map('n', '<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+                map('n', '<leader>ws', telescope_builtin.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
 
-                nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-                nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+                map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
+                map('n', '<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-                nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-                nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-                nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
+                map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                map('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
                 vim.api.nvim_buf_create_user_command(ev.buf, 'Format', function(_) vim.lsp.buf.format() end, { desc = 'Format current buffer with LSP' })
             end
