634c571d5acfec64b9aaa9c7389b87ed7ffb84a3
Prunkles <prunkles.dreemurr@gmail.com>
2025-04-05 09:18:24 +0300

    nvim: refactor(lspconfig): Use default lsp mappings from nvim 0.11.0

--- home/modules/nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/modules/nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -37,19 +37,20 @@ return {
                     vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = desc })
                 end
 
-                map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-                map('n', '<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction') -- TODO: v mode
+                map('n', 'grn', vim.lsp.buf.rename, 'Rename')
+                map('n', 'gra', vim.lsp.buf.code_action, 'Code Action') -- TODO: v mode
 
                 map('n', 'gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
                 map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-                map('n', 'gr', telescope_builtin.lsp_references, '[G]oto [R]erefences')
-                map('n', 'gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
+                map('n', 'grr', telescope_builtin.lsp_references, 'Goto Rerefences')
+                map('n', 'gri', telescope_builtin.lsp_implementations, 'Goto Implementation')
                 map('n', '<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
-                map('n', '<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+                map('n', 'gO', telescope_builtin.lsp_document_symbols, 'Document Symbols')
                 map('n', '<leader>ws', telescope_builtin.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
 
                 map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
                 map('n', '<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+                map({ 'i', 's' }, '<C-s>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
                 map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
                 map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
