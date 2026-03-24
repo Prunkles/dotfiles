37f40708e6f6719c073f781c6b0e006e20c0898b
Prunkles <prunkles@pawuq.net>
2026-03-24 14:58:11 +0200

    home: zsh: Opt-out dotnet cli telemetry

--- modules/home/zsh/_config/zsh/rc.d/50-dotnet.zsh
+++ modules/home/zsh/_config/zsh/rc.d/50-dotnet.zsh
@@ -1,6 +1,8 @@
 
 export PATH="$PATH:$HOME/.dotnet/tools"
 export DOTNET_NEW_PREFERRED_LANG="F#"
+# https://aka.ms/dotnet-cli-telemetry
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 alias fsi="dotnet fsi --nologo --consolecolors+"
 
